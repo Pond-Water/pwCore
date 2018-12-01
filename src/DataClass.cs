efb86cac3758fb58bee3575215ec41f3b350adff
Brian Carter <briancarter@chipsofttech.com>
2018-12-01 11:44:29 -0500

    Initial Checkin

---
+++ src/DataClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PWCore
+{
+    [GeneratedController("api/book")]
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+    }
+
+
+    [GeneratedController("api/v1/album")]
+    public class Album
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+    }
+
+}
---
+++ src/RemoteControllerFeatureProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using PWCore.Controllers;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace PWCore
+{
+    public class RemoteControllerFeatureProvider : IApplicationFeatureProvider<ControllerFeature>
+    {
+        public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature)
+        {
+            var remoteCode = new HttpClient().GetStringAsync("https://gist.githubusercontent.com/briancartercst/d4ba6b60c34675b131a99779a535adc3/raw/737a32274fc5f5244816c97dda4fadff0c46077b/types.txt").GetAwaiter().GetResult();
+
+            if (remoteCode != null)
+            {
+                var compilation = CSharpCompilation.Create("DynamicAssembly", new[] { CSharpSyntaxTree.ParseText(remoteCode) },
+                    new[] {
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(RemoteControllerFeatureProvider).Assembly.Location)
+                    },
+                    new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+                using (var ms = new MemoryStream())
+                {
+                    var emitResult = compilation.Emit(ms);
+
+                    if (!emitResult.Success)
+                    {
+                        // handle, log errors etc
+                        Debug.WriteLine("Compilation failed!");
+                        return;
+                    }
+
+                    ms.Seek(0, SeekOrigin.Begin);
+                    var assembly = Assembly.Load(ms.ToArray());
+                    var candidates = assembly.GetExportedTypes();
+
+                    foreach (var candidate in candidates)
+                    {
+                        feature.Controllers.Add(typeof(BaseController<>).MakeGenericType(candidate).GetTypeInfo());
+                    }
+                }
+            }
+        }
+    }
+}
